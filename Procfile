0f9c688f328f8c728104443e073225d2d9798d54
benderTheCrime <joseph.groseclose@spongecell.com>
2015-10-27 21:38:27 -0400

    Modified requirements, added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python nfldata.py
--- nfldata.py
+++ nfldata.py
@@ -35,4 +35,5 @@ def hello_world():
     return False
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
