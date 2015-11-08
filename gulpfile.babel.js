3e9ed99b08703e23da9cdac44cf930452f955b7d
benderTheCrime <joseph.groseclose@spongecell.com>
2015-11-08 18:12:25 -0500

    added webpack task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,6 +3,39 @@
 import webpack from  'webpack'
 
 
-gulp.task('webpack', function() {
-    
+gulp.task('webpack', function(cb) {
+    webpack({
+        entry: './src/play.es6',
+        module: [
+            {
+                test: /\.json$/,
+                loader: 'json-loader'
+            },
+            {
+                test: /\.coffee$/,
+                loader: 'coffee-loader'
+            },
+            {
+                test: /\.es6$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.s?css$/,
+                loader: 'style!css!sass'
+            }
+        ],
+        resolve: {},
+        output: {
+            path: './static/js',
+            filename: 'play.js'
+        }
+    }, function(e) {
+        if (e) {
+            throw new Error(e);
+        }
+
+        gulp.src('./static/play.js')
+            .pipe(uglify({ mangle: false }))
+            .pipe(gulp.dest('./static/play.js').on('end', cb));
+    });
 });
